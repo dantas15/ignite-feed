3a242c75c4ce8af6bbae8c16cb34832bb59d101f
Gustavo Galote <gusgalot3g@gmail.com>
2022-06-08 07:41:53 -0300

    feat: add App grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Header } from './components/Header';
 
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export const App = () => {
   return (
     <>
       <Header />
-      <h1>Test</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>Teste</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+            facere similique labore cumque? Aut hic recusandae, aliquam ad
+            facilis perspiciatis explicabo animi et porro! Tenetur, similique.
+            Veritatis, praesentium corrupti. Aliquam.
+          </p>
+        </main>
+      </div>
     </>
   );
 };
